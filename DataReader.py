09a8b2a85b8b0164ea16acc84fa325bf28de7d91
cherry-lime <hatorisugawara@gmail.com>
2021-02-12 16:30:08 +0100

    Split the code into functions

--- DataReader.py
+++ DataReader.py
@@ -2,10 +2,30 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 
-# Read input
-dataset = input("Please enter the path of the CSV file: ")
-output_name = input("Please enter the name (and path) of the output file: ")
-df = pd.read_csv(dataset)
+
+def process_data():
+    # Read input
+    dataset = input("Please enter the path of the CSV file: ")
+    output_name = input("Please enter the name (and path) of the output file: ")
+    df = pd.read_csv(dataset)
+
+    # Parse the timestamp and convert it into y-m-d form
+    df['event time:timestamp'] = pd.to_datetime(df['event time:timestamp'], format = '%d-%m-%Y %H:%M:%S.%f')
+
+    # Sort data by timestamp in ascending order
+    df.sort_values(['event time:timestamp'], axis=0, inplace=True)
+    
+    # split into train set and test set (80/20)
+    df_train, df_test = train_test_split(df, test_size=0.2, shuffle = False)
+
+    # Reset index
+    df_train.reset_index(drop=True, inplace=True)
+    df_test.reset_index(drop=True, inplace=True)
+
+    return (df_train, df_test, output_name)
+
+# Assign position number to each event
+#df_sort = df.set_index(df.groupby('case concept:name').cumcount(), append = True)
 
 #csv_train = input("Please enter the path of the training set (CSV file): ")
 #csv_test = input("Please enter the path of the test set (CSV file): ")
@@ -16,22 +36,6 @@ df = pd.read_csv(dataset)
 #frames = [df_train, df_test]
 #df = pd.concat(frames)
 #df = df.reset_index(drop=True, inplace=True)
-
-# Parse the timestamp and convert it into y-m-d form
-df['event time:timestamp'] = pd.to_datetime(df['event time:timestamp'], format = '%d-%m-%Y %H:%M:%S.%f')
-
-# Sort data by timestamp in ascending order
-df.sort_values(['event time:timestamp'], axis=0, inplace=True)
-
-# split into train set and test set (80/20)
-df_train, df_test = train_test_split(df, test_size=0.2, shuffle = False)
-
-# Reset index
-df_train.reset_index(drop=True, inplace=True)
-df_test.reset_index(drop=True, inplace=True)
-
-# Assign position number to each event
-df_sort = df.set_index(df.groupby('case concept:name').cumcount(), append = True)
 
 # Calculate the most common activity at each position
 #df_new = df_sort.groupby(level=1)["event concept:name"].transform(lambda x: x.value_counts().index[0])
@@ -81,7 +85,9 @@ def make_event_pred(df_train, df_test):
     
     return preds #test
 
-result = make_event_pred(df_train, df_test)
+def save_results(output_name):
+    result.to_csv(output_name + ".csv")
 
-# Save the output
-result.to_csv(output_name + ".csv")
+df_train, df_test, output_name = process_data()
+result = make_event_pred(df_train, df_test)
+save_results(output_name)
